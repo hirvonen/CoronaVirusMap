fbd3d71bf74e5924f6beb18fa889bd14c84ea021
hirvonen <thegoalkeeper@icloud.com>
2020-04-03 10:36:31 +0800

    Change format
    1.Change popup(show str while click) to tooltip(show str while mouse move)
    2.Change default scale

--- Corona_Map.py
+++ Corona_Map.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Ver 0.1
+# Ver 0.2
 
 import numpy as np
 import folium
@@ -9,21 +9,18 @@ from folium.plugins import HeatMap
 import json
 import urllib.request
 
-# GPS of GUANJIE Building(No.668, Shenchang Road, Minhang District, Shanghai, China)
+# GPS of current location
 latitude = 31.19
 longitude = 121.32
-map_data = folium.Map(location=[latitude, longitude], zoom_start=12)
-
+map_data = folium.Map(location=[latitude, longitude], zoom_start=4)
 
 # Read Dataset
 resp = urllib.request.urlopen('https://coronavirus-tracker-api.herokuapp.com/v2/locations')
 org_data = (json.loads(resp.read()))['locations']
 json_data = pd.json_normalize(org_data)
 
-
 # Instantiate a feature group for the incidents in the dataframe
 incidents = folium.map.FeatureGroup()
-
 
 # Add circle on the map
 for country_no in json_data.id:
@@ -40,21 +37,28 @@ for country_no in json_data.id:
     )
 map_data.add_child(incidents)
 
-
 # Add pop-up text to circles
 for country_no in json_data.id:
-    label_provinceName = json_data.province[country_no]
-    label_countryName = json_data.country[country_no]
-    label_confirmNo = json_data["latest.confirmed"][country_no]
-    if label_provinceName != '':
-        label = str(label_provinceName) + ':' + str(label_confirmNo)
+    if json_data.province[country_no] != '':
+        label_tooltip = "{}{}{}{}{}".format(
+            str(json_data.province[country_no]),
+            ' CONFIRM:',
+            str(json_data["latest.confirmed"][country_no]),
+            ' DEATH:',
+            str(json_data["latest.deaths"][country_no]))
     else:
-        label = str(label_countryName) + ':' + str(label_confirmNo)
-    folium.Marker(
+        label_tooltip = "{}{}{}{}{}".format(
+            str(json_data.country[country_no]),
+            ' CONFIRM:',
+            str(json_data["latest.confirmed"][country_no]),
+            ' DEATH:',
+            str(json_data["latest.deaths"][country_no]))
+    folium.RegularPolygonMarker(
         [json_data["coordinates.latitude"][country_no],
          json_data["coordinates.longitude"][country_no]],
-        popup=label).add_to(map_data)
-
+        tooltip=label_tooltip,
+        number_of_sides=10,
+        radius=5).add_to(map_data)
 
 # Convert data format
 lat = np.array(json_data["coordinates.latitude"][0:255], dtype=float)
@@ -62,9 +66,7 @@ lon = np.array(json_data["coordinates.longitude"][0:255], dtype=float)
 confirm = np.array(json_data["latest.confirmed"][0:255], dtype=float)
 heatdata = [[lat[i], lon[i], confirm[i]] for i in range(255)]
 
-
 # add incidents to map
-HeatMap(heatdata).add_to(map_data)
+HeatMap(heatdata, radius=40).add_to(map_data)
 
-
-map_data.save(os.path.join(r'/Users/timmyzhang/PycharmProjects/CoronaVirusMap/', "Heatmap.html"))
+map_data.save(os.path.join(r'F:\python\Folium', "Heatmap.html"))
